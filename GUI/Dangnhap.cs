e8263494e7a2fa9c00e23cc340f23356315b50bd
Thao2303 <145946898+Thao2303@users.noreply.github.com>
2023-11-09 22:52:58 +0700

    Add project files.

---
+++ GUI/Dangnhap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cosmetics.GUI;
+
+namespace Cosmetics.GUI
+{
+    public partial class Dangnhap : Form
+    {
+        public Dangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            Form1 trangchu = new Form1();
+            trangchu.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ GUI/Trangchu.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace Cosmetics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btQLSP = new System.Windows.Forms.Button();
+            this.btQLNV = new System.Windows.Forms.Button();
+            this.btQLKH = new System.Windows.Forms.Button();
+            this.btQLTK = new System.Windows.Forms.Button();
+            this.btLichsu = new System.Windows.Forms.Button();
+            this.btSaoluu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btMuahang = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuHeThong = new System.Windows.Forms.ToolStripMenuItem();
+            this.đổiMậtKhẩuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(1, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(246, 146);
+            this.panel1.TabIndex = 0;
+            // 
+            // btQLSP
+            // 
+            this.btQLSP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btQLSP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btQLSP.Image = ((System.Drawing.Image)(resources.GetObject("btQLSP.Image")));
+            this.btQLSP.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btQLSP.Location = new System.Drawing.Point(15, 206);
+            this.btQLSP.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.btQLSP.Name = "btQLSP";
+            this.btQLSP.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btQLSP.Size = new System.Drawing.Size(232, 52);
+            this.btQLSP.TabIndex = 1;
+            this.btQLSP.Text = "Quản lý sản phẩm";
+            this.btQLSP.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btQLSP.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btQLSP.UseVisualStyleBackColor = false;
+            // 
+            // btQLNV
+            // 
+            this.btQLNV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btQLNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btQLNV.Image = ((System.Drawing.Image)(resources.GetObject("btQLNV.Image")));
+            this.btQLNV.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btQLNV.Location = new System.Drawing.Point(15, 254);
+            this.btQLNV.Name = "btQLNV";
+            this.btQLNV.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btQLNV.Size = new System.Drawing.Size(232, 56);
+            this.btQLNV.TabIndex = 1;
+            this.btQLNV.Text = "Quản lý nhân viên";
+            this.btQLNV.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btQLNV.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btQLNV.UseVisualStyleBackColor = false;
+            // 
+            // btQLKH
+            // 
+            this.btQLKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btQLKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btQLKH.Image = ((System.Drawing.Image)(resources.GetObject("btQLKH.Image")));
+            this.btQLKH.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btQLKH.Location = new System.Drawing.Point(15, 305);
+            this.btQLKH.Margin = new System.Windows.Forms.Padding(10, 3, 3, 3);
+            this.btQLKH.Name = "btQLKH";
+            this.btQLKH.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btQLKH.Size = new System.Drawing.Size(232, 55);
+            this.btQLKH.TabIndex = 1;
+            this.btQLKH.Text = "Quản lý khách hàng";
+            this.btQLKH.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btQLKH.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btQLKH.UseVisualStyleBackColor = false;
+            // 
+            // btQLTK
+            // 
+            this.btQLTK.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btQLTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btQLTK.Image = ((System.Drawing.Image)(resources.GetObject("btQLTK.Image")));
+            this.btQLTK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btQLTK.Location = new System.Drawing.Point(15, 356);
+            this.btQLTK.Name = "btQLTK";
+            this.btQLTK.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btQLTK.Size = new System.Drawing.Size(232, 55);
+            this.btQLTK.TabIndex = 1;
+            this.btQLTK.Text = "Quản lý tài khoản";
+            this.btQLTK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btQLTK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btQLTK.UseVisualStyleBackColor = false;
+            // 
+            // btLichsu
+            // 
+            this.btLichsu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btLichsu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btLichsu.Image = ((System.Drawing.Image)(resources.GetObject("btLichsu.Image")));
+            this.btLichsu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btLichsu.Location = new System.Drawing.Point(15, 406);
+            this.btLichsu.Name = "btLichsu";
+            this.btLichsu.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btLichsu.Size = new System.Drawing.Size(232, 53);
+            this.btLichsu.TabIndex = 1;
+            this.btLichsu.Text = "Lịch sử bán";
+            this.btLichsu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btLichsu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btLichsu.UseVisualStyleBackColor = false;
+            // 
+            // btSaoluu
+            // 
+            this.btSaoluu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btSaoluu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btSaoluu.Image = ((System.Drawing.Image)(resources.GetObject("btSaoluu.Image")));
+            this.btSaoluu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btSaoluu.Location = new System.Drawing.Point(15, 455);
+            this.btSaoluu.Name = "btSaoluu";
+            this.btSaoluu.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btSaoluu.Size = new System.Drawing.Size(232, 56);
+            this.btSaoluu.TabIndex = 1;
+            this.btSaoluu.Text = "Sao lưu dữ liệu";
+            this.btSaoluu.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btSaoluu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btSaoluu.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightPink;
+            this.label1.Location = new System.Drawing.Point(374, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 99);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Rose Cosmetics";
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(273, 159);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(724, 352);
+            this.panel2.TabIndex = 3;
+            // 
+            // btMuahang
+            // 
+            this.btMuahang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btMuahang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btMuahang.Image = ((System.Drawing.Image)(resources.GetObject("btMuahang.Image")));
+            this.btMuahang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btMuahang.Location = new System.Drawing.Point(15, 159);
+            this.btMuahang.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.btMuahang.Name = "btMuahang";
+            this.btMuahang.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btMuahang.Size = new System.Drawing.Size(232, 52);
+            this.btMuahang.TabIndex = 1;
+            this.btMuahang.Text = "Hóa đơn mua hàng";
+            this.btMuahang.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btMuahang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btMuahang.UseVisualStyleBackColor = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Pink;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuHeThong,
+            this.aboutUsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1110, 30);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuHeThong
+            // 
+            this.mnuHeThong.BackColor = System.Drawing.Color.Transparent;
+            this.mnuHeThong.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đổiMậtKhẩuToolStripMenuItem,
+            this.đăngXuấtToolStripMenuItem});
+            this.mnuHeThong.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.mnuHeThong.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.mnuHeThong.Name = "mnuHeThong";
+            this.mnuHeThong.Size = new System.Drawing.Size(87, 26);
+            this.mnuHeThong.Text = "Tài khoản";
+            // 
+            // đổiMậtKhẩuToolStripMenuItem
+            // 
+            this.đổiMậtKhẩuToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.đổiMậtKhẩuToolStripMenuItem.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.đổiMậtKhẩuToolStripMenuItem.Name = "đổiMậtKhẩuToolStripMenuItem";
+            this.đổiMậtKhẩuToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.đổiMậtKhẩuToolStripMenuItem.Text = "Đổi mật khẩu";
+            // 
+            // đăngXuấtToolStripMenuItem
+            // 
+            this.đăngXuấtToolStripMenuItem.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            this.đăngXuấtToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.đăngXuấtToolStripMenuItem.Text = "Đăng xuất";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.aboutUsToolStripMenuItem.ForeColor = System.Drawing.Color.PaleVioletRed;
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(82, 26);
+            this.aboutUsToolStripMenuItem.Text = "About us";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1110, 610);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btSaoluu);
+            this.Controls.Add(this.btLichsu);
+            this.Controls.Add(this.btQLTK);
+            this.Controls.Add(this.btQLKH);
+            this.Controls.Add(this.btQLNV);
+            this.Controls.Add(this.btMuahang);
+            this.Controls.Add(this.btQLSP);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Trang chủ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btQLSP;
+        private System.Windows.Forms.Button btQLNV;
+        private System.Windows.Forms.Button btQLKH;
+        private System.Windows.Forms.Button btQLTK;
+        private System.Windows.Forms.Button btLichsu;
+        private System.Windows.Forms.Button btSaoluu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btMuahang;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuHeThong;
+        private System.Windows.Forms.ToolStripMenuItem đổiMậtKhẩuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+    }
+}
+
